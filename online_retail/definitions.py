c0386eaee4076c5ee24f1d3d9e1f92df0b3b050a
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2025-07-24 11:45:23 -0300

    feat: enhance definitions with MLflow tracking and additional clustering assets

--- online_retail/definitions.py
+++ online_retail/definitions.py
@@ -1,6 +1,61 @@
 from dagster import Definitions, load_assets_from_modules
-from online_retail.assets import clean_dataset, load_dataset, create_features
+from dagster_mlflow import mlflow_tracking
+import os
+from dotenv import load_dotenv
+from online_retail.assets import (
+    agglomerative,
+    clean_dataset,
+    dbscan,
+    gaussian_mixture,
+    load_dataset,
+    create_features,
+    kmeans,
+    summarize,
+)
 
-assets = load_assets_from_modules([load_dataset, clean_dataset, create_features])
+# Load environment variables from .env file
+load_dotenv()
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "file:./mlruns")
 
-defs = Definitions(assets=assets)
+assets = load_assets_from_modules(
+    [
+        load_dataset,
+        clean_dataset,
+        create_features,
+        kmeans,
+        agglomerative,
+        gaussian_mixture,
+        dbscan,
+        summarize,
+    ]
+)
+
+defs = Definitions(
+    assets=assets,
+    resources={
+        "mlflow_kmeans": mlflow_tracking.configured(
+            {
+                "mlflow_tracking_uri": MLFLOW_TRACKING_URI,
+                "experiment_name": "kmeans_experiment1",
+            }
+        ),
+        "mlflow_agglomerative": mlflow_tracking.configured(
+            {
+                "mlflow_tracking_uri": MLFLOW_TRACKING_URI,
+                "experiment_name": "agglomerative_experiment",
+            }
+        ),
+        "mlflow_gaussian_mixture": mlflow_tracking.configured(
+            {
+                "mlflow_tracking_uri": MLFLOW_TRACKING_URI,
+                "experiment_name": "gaussian_mixture_experiment",
+            }
+        ),
+        "mlflow_dbscan": mlflow_tracking.configured(
+            {
+                "mlflow_tracking_uri": MLFLOW_TRACKING_URI,
+                "experiment_name": "dbscan_experiment",
+            }
+        ),
+    },
+)
